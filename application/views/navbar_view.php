969269f148bc27c750a9f491c58fe86876efbf5a
unknown <quuxc@ya.ru>
2014-07-21 20:52:53 +0600

    last5

--- application/views/navbar_view.php
+++ application/views/navbar_view.php
@@ -23,7 +23,7 @@
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Language<span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
-								<li><a href="/ru">Russian</a></li>
+								<li><a href="/ru">Русский</a></li>
 								<li><a href="/en">English</a></li>
 							</ul>
 						</li>
